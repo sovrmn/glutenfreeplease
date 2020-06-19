3e8e7b4cf369b04e7dbc0fa97ed48742e72563fa
Sovana Romano <sovana_srinn@hotmail.com>
2020-06-20 01:46:49 +0200

    create a recipe model

---
+++ app/models/recipe.rb
@@ -0,0 +1,4 @@
+class Recipe < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true, uniqueness: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_005003) do
+ActiveRecord::Schema.define(version: 2020_06_19_234229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_06_15_005003) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "recipes", "users"
 end
