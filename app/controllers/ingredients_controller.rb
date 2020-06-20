e307823d2c899bbdd7cffa83bacd5ba943f0f9a8
Sovana Romano <sovana_srinn@hotmail.com>
2020-06-20 03:06:57 +0200

    add ingredients controller and views

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,29 @@
+class IngredientsController < ApplicationController
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def show
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to ingredient_path(@ingredient)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :description)
+  end
+end
+
