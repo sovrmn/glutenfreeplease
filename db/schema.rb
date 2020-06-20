dfab755f8cf0891c3bbf83dc955942ab84f6b2c0
Sovana Romano <sovana_srinn@hotmail.com>
2020-06-20 02:42:12 +0200

    create a model iongredient

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_19_234229) do
+ActiveRecord::Schema.define(version: 2020_06_20_003641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_ingredients_on_user_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
@@ -40,5 +49,6 @@ ActiveRecord::Schema.define(version: 2020_06_19_234229) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "ingredients", "users"
   add_foreign_key "recipes", "users"
 end
