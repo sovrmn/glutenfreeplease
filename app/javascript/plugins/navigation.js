8f253bc3bca09d0ce477c834ed06c236f99aa36e
Sovana Romano <sovana_srinn@hotmail.com>
2020-07-06 02:28:53 +0200

    corrected js navigation again

--- app/javascript/plugins/navigation.js
+++ app/javascript/plugins/navigation.js
@@ -5,19 +5,17 @@ const navigation = () => {
 
   menuItems.forEach((menuItem) =>{
     menuItem.addEventListener('click', (event) =>{
-    // event.preventDefault() ;
+      // event.preventDefault() ;
       // console.log(event.currentTarget);
       if (event.currentTarget.classList.contains('active')) {
-
         console.log("you are already in the right menu element!");
-        } else {
-          console.log("you are in another menu element!");
-          document.querySelector(".active").classList.remove('active');
-          event.currentTarget.classList.add('active');
+      } else {
+        console.log("you are in another menu element!");
+        document.querySelector(".active").classList.remove('active');
+        event.currentTarget.classList.add('active');
         }
-      });
-  };
-
-
+    });
+  });
+};
 
 export {navigation};
