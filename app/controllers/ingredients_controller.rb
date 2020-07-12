d4d74d6fafc1d9fa55f0345fb9686e607f12562b
Sovana Romano <sovana_srinn@hotmail.com>
2020-07-12 14:40:33 +0200

    add edit and destroy to ingredients controller and fine tune footer and navbar

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -22,10 +22,33 @@ class IngredientsController < ApplicationController
     end
   end
 
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.user = current_user
+    @ingredient.update(ingredient_params)
+    if @ingredient.save
+      redirect_to ingredients_path
+    else
+      render :new
+      flash[:notice] = "The modification could not be completed."
+
+    end
+  end
+
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.destroy
+
+    redirect_to ingredients_path
+  end
+
   private
 
   def ingredient_params
     params.require(:ingredient).permit(:name, :description)
   end
 end
-
