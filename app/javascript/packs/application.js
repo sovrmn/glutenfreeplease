ad2a6e90f8831699e42044a707616df664757b02
Sovana Romano <sovana_srinn@hotmail.com>
2020-11-19 18:50:18 +0100

    corrected css of page 404

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,7 +25,6 @@ require("channels")
 // External imports
 import "bootstrap";
 import {navigation} from "../plugins/navigation";
-import {navigation} from "../plugins/error404";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -33,5 +32,4 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
     navigation;
-    error404;
   });
